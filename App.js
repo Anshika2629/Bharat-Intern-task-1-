4ca8441cbc1cdbb63e1df40933317e8b05e07a6c
Anshika2629 <136101029+Anshika2629@users.noreply.github.com>
2023-11-09 11:48:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+
+import Netflix from './Netflix/Netflix';
+
+function App() {
+  return (
+    <div >
+      
+    <Netflix/>
+
+
+    </div>
+  );
+}
+
+export default App;
+
